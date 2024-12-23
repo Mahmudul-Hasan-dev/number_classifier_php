34386fa2323f217b848b398efd71e4c41fe7395e
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2024-12-23 12:20:12 +0600

    classifier number

---
+++ number_classifier.php
@@ -0,0 +1,13 @@
+<?php
+
+$number = 5; // Change this number to test 
+
+// Step 2: Classify  using if-else
+if ($number > 0) {
+    echo "The number $number is Positive.\n";
+} elseif ($number < 0) {
+    echo "The number $number is Negative.\n";
+} else {
+    echo "The number is Zero.\n";
+}
+?>
